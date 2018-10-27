da06056d8a4f935a96ede8cea1b1191349cb3bc3
liumapp <liumapp.com@gmail.com>
2018-10-27 16:25:13 +0800

    add entry js file and timerbtn component

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * @file index.js
+ * @author liumapp
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 2018/10/27
+ */
+export { default } from './timerBtn.vue'
---
+++ timerBtn.vue
@@ -0,0 +1,12 @@
+/**
+ * @file timerBtn.vue
+ * @author liumapp
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 2018/10/27
+ */
+<template>
+</template>
+<script>
+
+</script>
